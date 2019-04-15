cf07515b20df8565792b3c834cdf0d529eaad2a0
Onedean <1209062975@qq.com>
2019-04-15 23:48:39 +0800

    分而治之递归法求最大子列

--- Mooc/最大子列和问题.c
+++ Mooc/最大子列和问题.c
@@ -46,8 +46,60 @@ int maxSubArray2(int *nums, int numsSize)
 }
 
 // 方法三：递归分而治之法,时间复杂度O(n*log(n))
-// todo
+int max(int a, int b, int c)
+{ /* 返回3个整数中的最大值 */
+    return a > b ? a > c ? a : c : b > c ? b : c;
+}
 
+int DivideAndConquer(int List[], int left, int right)
+{                                            /* 分治法求List[left]到List[right]的最大子列和 */
+    int MaxLeftSum, MaxRightSum;             /* 存放左右子问题的解 */
+    int MaxLeftBorderSum, MaxRightBorderSum; /*存放跨分界线的结果*/
+
+    int LeftBorderSum, RightBorderSum;
+    int center, i;
+
+    if (left == right)
+    { /* 递归的终止条件，子列只有1个数字 */
+        if (List[left] > 0)
+            return List[left];
+        else
+            return 0;
+    }
+
+    /* 下面是"分"的过程 */
+    center = (left + right) / 2; /* 找到中分点 */
+    /* 递归求得两边子列的最大和 */
+    MaxLeftSum = DivideAndConquer(List, left, center);
+    MaxRightSum = DivideAndConquer(List, center + 1, right);
+
+    /* 下面求跨分界线的最大子列和 */
+    MaxLeftBorderSum = 0;
+    LeftBorderSum = 0;
+    for (i = center; i >= left; i--)
+    { /* 从中线向左扫描 */
+        LeftBorderSum += List[i];
+        if (LeftBorderSum > MaxLeftBorderSum)
+            MaxLeftBorderSum = LeftBorderSum;
+    } /* 左边扫描结束 */
+
+    MaxRightBorderSum = 0;
+    RightBorderSum = 0;
+    for (i = center + 1; i <= right; i++)
+    { /* 从中线向右扫描 */
+        RightBorderSum += List[i];
+        if (RightBorderSum > MaxRightBorderSum)
+            MaxRightBorderSum = RightBorderSum;
+    } /* 右边扫描结束 */
+
+    /* 下面返回"治"的结果 */
+    return max(MaxLeftSum, MaxRightSum, MaxLeftBorderSum + MaxRightBorderSum);
+}
+
+int maxSubseqSum3(int List[], int N)
+{ /* 保持与前2种算法相同的函数接口 */
+    return DivideAndConquer(List, 0, N - 1);
+}
 
 //方法四：在线更新法，时间复杂度O(n)
 int maxSubArray4(int *nums, int numsSize)
